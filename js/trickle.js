32a34d530588c05578ba03c5ba6687f93f2a2429
FG Ribreau <github@fgribreau.com>
2010-12-28 15:10:45 +0100

    Some cleaning + Ucfirst

--- js/trickle.js
+++ js/trickle.js
@@ -1,50 +1,47 @@
+//Quick & Dirty code ! 
+//You've been warned !
+
 var Trickle = {
 	_keywords:null,
 	_ws:'http://search.twitter.com/search?q={keywords}&refresh=true&since_id={since_id}&callback=?',
 	_sinceId:0,
 	_tweets:null,
-	_uiTweets:null,
-	w:0,//Doc width
-	h:0,//Doc height
+	
+	//jQUery object cache
+	$tweets:null,
+	$doc:null,
+	
+	//Doc width
+	w:0,
+	//Doc height
+	h:0,
+	
 	i:0,
+	
 	PROD:false,
 	
 	init:function(){
-		$('.iForm').bind('submit',$.proxy(this.onSubmit, this));
-		var $doc = $(document);
-	
-		this.w = $doc.width();
-		this.h = $doc.height();
-		this._setCSSSize();
+	  //Bind the form
+		$('.iForm').bind('submit', $.proxy(this.onSubmit, this));
 		
-		this._uiTweets = $('.tweets');
+		//jQuery cache
+		this.$doc = $(document);
+		this.$tweets = $('.tweets');
+		
+		
+		this.w = this.$doc.width();
+		this.h = this.$doc.height();
+		this._UIsetCSSSize();
 		
 		//Compile the template
 		$( "#tweetTemplate" ).template( "tweetTpl" );
-		//debug
-		//$('.iForm').submit();
-		
+
+    //Bind on "OrientationChange" event
 		window.onorientationchange = function() {
-			var $doc = $doc = $(document);
-			Trickle.w = $doc.width();
-			Trickle.h = $doc.height();
-			Trickle._setCSSSize();
+			Trickle.w = this.$doc.width();
+			Trickle.h = this.$doc.height();
+			Trickle._UIsetCSSSize();
 		}
-	},
-	
-	_setCSSSize:function(){
-		if($('#headCssSize').length >= 1){
-			$('#headCssSize').remove();
-		}
-		
-		$(document).find('head').append('<style id="headCssSize" type="text/css">.tweets ul{width:'+this.w*2+'px;height:'+this.h+'px}.tweets ul li{width:'+this.w+'px;height:'+this.h+'px}</style>');
-	},
-	
-	debug:function(){
-		if(!this.PROD)
-			return;
-		
-		console.debug(arguments);
 	},
 	
 	onSubmit:function(e){
@@ -57,7 +54,7 @@ var Trickle = {
 		}
 		
 		$('.iForm').hide();
-		$('.tweets ul').show();
+		this.$tweets.find('ul').show();
 		
 		this.getTweets();
 	},
@@ -70,33 +67,39 @@ var Trickle = {
 					.replace('{keywords}', encodeURIComponent(this._keywords))
 					.replace('{since_id}', this._sinceId),
 			dataType: 'json',
-			success: $.proxy(_onSuccess, this),
+			
+			success: $.proxy(this.getTweets_onSuccess, this),
 			error:function(XMLHttpRequest, textStatus, errorThrown){
 				alert('Twitter API Error: '+ textStatus);
 			}
 		});
 		
-		function _onSuccess(data){
-			this._sinceId = data.since_id;	
-			window.data = data;
-			if(data.results.length > 1){	
-				this.debug('data Ok', data);
-				this._tweets = data.results;
-				this._preloadNextTweet();
-				this.nextTweet();
-			} else {
-				//Attendre X secondes avant de redemander les tweets
-				if(this._tweets == null){
-					alert('Nothing found. Try another keywords');
-					$('.iForm').show();
-					$('.tweets ul').hide();
-					return;
-				}
-				var ctx = this;
-				setTimeout(function(){ctx.getTweets();}, 5000);
-			}
-		}
+	},
+	
+	getTweets_onSuccess: function(data){
+		this._sinceId = data.since_id;	
 		
+		if(data.results.length > 1){	
+			this._debug('data Ok', data);
+			this._tweets = data.results;
+			
+			this._preloadNextTweet();
+			this.nextTweet();
+			
+		} else {
+			
+			if(this._tweets == null){
+				alert('Nothing found. Try another keywords');
+				
+				$('.iForm').show();
+				this.$tweets.find('ul').hide();
+				return;
+			}
+			
+			//Wait 5sec before restarting requests.
+			var ctx = this;
+			setTimeout(function(){ctx.getTweets();}, 5000);
+		}
 	},
 	
 	//Charge dans le second LI le tweet
@@ -105,9 +108,9 @@ var Trickle = {
 			return false;
 		}
 		
-		this._uiTweets.find('li:last').html($.tmpl("tweetTpl", {
-			text:this._tweets[this.i].text,
-			screen_name:this._tweets[this.i].from_user
+		this.$tweets.find('li:last').html($.tmpl("tweetTpl", {
+			  text:       this._ucFirst(this._tweets[this.i].text)
+			, screen_name:this._tweets[this.i].from_user
 		}));
 		
 		this.i++;
@@ -135,5 +138,30 @@ var Trickle = {
 			ctx._replaceTweet();
 			ctx.nextTweet();
 		}, 9000);
+	},
+	
+	/**
+	  
+	**/
+	_UIsetCSSSize:function(){
+		if($('#headCssSize').length >= 1){
+			$('#headCssSize').remove();
+		}
+		
+		this.$doc.find('head').append('<style id="headCssSize" type="text/css">.tweets ul{width:'+this.w*2+'px;height:'+this.h+'px}.tweets ul li{width:'+this.w+'px;height:'+this.h+'px}</style>');
+	},
+	
+	/**
+	HELPER
+	**/
+	_ucFirst:function(str){
+	  return str.charAt(0).toUpperCase() + str.substr(1);
+	},
+	
+	_debug:function(){
+		if(!this.PROD)
+			return;
+		
+		console.debug(arguments);
 	}
 };
